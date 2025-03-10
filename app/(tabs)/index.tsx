77138e0e729756b16757d95e7ca1d0713eaefde3
Soham Kolhatkar <sohamk130405@gmail.com>
2025-03-10 21:04:26 +0530

    Home screen basic ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,29 @@
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import React from "react";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
+import { useRouter } from "expo-router";
 
 const Index = () => {
+  const router = useRouter();
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-4xl text-primary font-bold">Welcome!</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image source={icons.logo} className="w-12 h-12 mt-20 mb-5 mx-auto" />
+        <View className="flex-1 mt-5">
+          <SearchBar
+          onPress={()=>router.push("/search")}
+          placeholder="Search for a movie"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
